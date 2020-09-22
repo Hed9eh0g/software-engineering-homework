9a3f525d6c03ab5fb28efac2efaafe2a5ddac08c
Hed9eh0g <58317851+Hed9eh0g@users.noreply.github.com>
2020-09-22 21:35:11 +0800

    first commit

---
+++ 第一次作业/main.py
@@ -0,0 +1,55 @@
+import jieba
+import gensim
+import re
+
+
+def get_file_contents(path):
+    str = ''
+    f = open(path, 'r', encoding='UTF-8')
+    line = f.readline()
+    while line:
+        str = str + line
+        line = f.readline()
+    f.close()
+    return str
+
+
+def filter(str):
+    pattern = re.compile(u"[^a-zA-Z0-9\u4e00-\u9fa5]")
+    str = pattern.sub("", str)
+    result = jieba.lcut(str)
+    return result
+
+
+'''
+def calc_similarity(text1, text2):
+    text1 = [text1]
+    
+    dictionary = gensim.corpora.Dictionary(text1)
+    num_features = len(dictionary.token2id)
+    corpus = [dictionary.doc2bow(text) for text in text1]
+    new_vec = dictionary.doc2bow(text2)
+    similarity = gensim.similarities.Similarity('-Similarity-index', corpus, num_features)
+    return similarity[new_vec]
+'''
+
+
+def calc_similarity(text1, text2):
+    texts = [text1, text2]
+    dictionary = gensim.corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    similarity = gensim.similarities.Similarity('-Similarity-index', corpus, num_features=len(dictionary))
+    test_corpus_1 = dictionary.doc2bow(text1)
+    cosine_sim = similarity[test_corpus_1][1]
+    return cosine_sim
+
+
+if __name__ == '__main__':
+    path1 = "./test/orig.txt"
+    path2 = "./test/orig_0.8_dis_15.txt"
+    str1 = get_file_contents(path1)
+    str2 = get_file_contents(path2)
+    text1 = filter(str1)
+    text2 = filter(str2)
+    similarity = calc_similarity(text1, text2)
+    print("文章相似度： %.4f" % similarity)
