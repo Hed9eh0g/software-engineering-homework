1eeec65a1617e99f9e06526e16aa38392274334a
Hed9eh0g <58317851+Hed9eh0g@users.noreply.github.com>
2020-09-23 22:21:22 +0800

    Update main.py

--- 第一次作业/main.py
+++ 第一次作业/main.py
@@ -1,27 +1,26 @@
-#python 3.7
 import jieba
 import gensim
 import re
-
-
+ 
+#获取指定路径的文件内容
 def get_file_contents(path):
-    str = ''
+    string = ''
     f = open(path, 'r', encoding='UTF-8')
     line = f.readline()
     while line:
-        str = str + line
+        string = string + line
         line = f.readline()
     f.close()
-    return str
-
-
-def filter(str):
+    return string
+ 
+#将读取到的文件内容先把标点符号、转义符号等特殊符号过滤掉，然后再进行结巴分词
+def filter(string):
     pattern = re.compile(u"[^a-zA-Z0-9\u4e00-\u9fa5]")
-    str = pattern.sub("", str)
-    result = jieba.lcut(str)
+    string = pattern.sub("", string)
+    result = jieba.lcut(string)
     return result
-
-
+ 
+#传入过滤之后的数据，通过调用gensim.similarities.Similarity计算余弦相似度
 def calc_similarity(text1, text2):
     texts = [text1, text2]
     dictionary = gensim.corpora.Dictionary(texts)
@@ -30,17 +29,28 @@ def calc_similarity(text1, text2):
     test_corpus_1 = dictionary.doc2bow(text1)
     cosine_sim = similarity[test_corpus_1][1]
     return cosine_sim
-
-
-if __name__ == '__main__':
-    path1 = "./test/orig.txt"
-    path2 = "./test/orig_0.8_dis_15.txt"
-    save_path="./save.txt"
+ 
+ 
+def main_test():
+    path1 = input("输入论文原文的文件的绝对路径：")
+    path2 = input("输入抄袭版论文的文件的绝对路径：")
+    if os.path.exists(path1) :
+        print("论文原文文件不存在！")
+        exit()
+    if os.path.exists(path2):
+        print("抄袭版论文文件不存在！")
+        exit()
     str1 = get_file_contents(path1)
     str2 = get_file_contents(path2)
     text1 = filter(str1)
     text2 = filter(str2)
-    similarity = calc_similarity(text1, text2)
+    similarity = calc_similarity(text1, text2)   #生成的similarity变量类型为<class 'numpy.float32'>
+    result=round(similarity.item(),2)  #借助similarity.item()转化为<class 'float'>，然后再取小数点后两位
+    return result
+ 
+ 
+if __name__ == '__main__':
+    main_test()
     print("文章相似度： %.4f" % similarity)
     #将相似度结果写入指定文件
     f = open(save_path, 'w', encoding="utf-8")
